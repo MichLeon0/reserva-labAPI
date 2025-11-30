a1461c31410c67f5ccaa3335d0c634e2cde4b419
Angel <angel.gtz.pc.527@gmail.com>
2025-11-30 17:37:09 -0600

    Cambios

--- sistema_buap_api/settings.py
+++ sistema_buap_api/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '-_&+lsebec(whhw!%n@ww&1j=4-^j_if9x8$q778+99oz&!ms2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'reserva-labapi.onrender.com']
 
